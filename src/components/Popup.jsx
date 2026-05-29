b882741f12feecaf594e1e04bc58ae88ad98ee14
Gregory <chongweiyi20@gmail.com>
2026-05-30 05:50:33 +0800

    static version; correct word display

---
+++ src/components/Popup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Popup = () => {
+  return (
+    <div class="popup-container" id="popup-container">
+      <div class="popup">
+        <h2>id="finalMessage"</h2>
+        <h3>id="finalMessageRevealWord"</h3>
+        <button id="play-button">Play Again</button>
+      </div>
+    </div>
+  )
+}
+
+export default Popup
